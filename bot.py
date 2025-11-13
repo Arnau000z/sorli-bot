f4f2307d01ddbaa22be5e7d2d2a088c4d7a450e6
Arnau000z <131853666+Arnau000z@users.noreply.github.com>
2025-11-13 21:46:46 +0000

    todo bien

---
+++ bot.py
@@ -0,0 +1,9 @@
+# EJEMPLO BASE (sin selectores finales, solo estructura)
+import time
+import datetime
+
+def reservar():
+    print("Ejecutando bot...")
+
+if __name__ == "__main__":
+    reservar()
